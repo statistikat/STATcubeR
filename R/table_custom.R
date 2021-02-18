c1cabe17dded9681619b10ed0b0f0b5358fb3d3d
Gregor de Cillia <de.cillia.gregor@gmail.com>
2021-02-18 12:13:23 +0100

    rename param: fields -> dimensions
    - make sc_table_custom() more consistent
      with the json standard
    - mention in the documentation that the
      dimensions are referred to as "fields"
      in the parsed API response

--- R/table_custom.R
+++ R/table_custom.R
@@ -1,8 +1,9 @@
 #' @param db The uid of a database
 #' @param measures A character vector of uids for measures. Can be either of
 #'   type `MEASURE` or of type `STAT_FUNCTION`
-#' @param fields A character vector of field variables. Can be either of
-#'   type `FIELD` or type `VALUESET`
+#' @param dimensions A character vector of dimensions for the cube. Can be
+#'   either of type `FIELD` or type `VALUESET`. Those entries are referred to
+#'   as `fields` in the parsed API response
 #' @rdname sc_table
 #' @examples
 #'
@@ -12,16 +13,16 @@
 #'     "str:statfn:detouextregsai:F-DATA1:F-ANK:SUM",
 #'     "str:measure:detouextregsai:F-DATA1:F-UEB"
 #'   ),
-#'   fields = c(
+#'   dimensions = c(
 #'     "str:field:detouextregsai:F-DATA1:C-SDB_TIT-0",
 #'     "str:valueset:detouextregsai:F-DATA1:C-C93-2:C-C93SUM-0"
 #'   )
 #' )
 #' @export
-sc_table_custom <- function(db, measures, fields, language = c("en", "de"),
+sc_table_custom <- function(db, measures, dimensions, language = c("en", "de"),
                             key = sc_key()) {
   json_list <- list(database = db, measures = as.list(measures),
-                    dimensions = lapply(fields, list))
+                    dimensions = lapply(dimensions, list))
   response <- httr::POST(
     url = paste0(base_url, "/table"),
     body = jsonlite::toJSON(json_list, auto_unbox = TRUE),
