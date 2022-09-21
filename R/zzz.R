c5af0e032b44b007214041223c49585d87ef7f1d
Gregor de Cillia <de.cillia.gregor@gmail.com>
2022-09-21 15:15:49 +0200

    add a cli theme for {pkgdown}
    add a hook in .onLoad() which customizes the
    cli theme in case pkgown is running.
    Setting this here rather than in
    vignettes/R/df_print.R has the advantage that
    @examples are also affected by the styling
    
    - options(cli.theme) is currently used only for
      colors
    - hyperlink support is enabled even if the console
      doesn't support it because after the conversion
      into HTML everything is fine
    - The same is true for the limit of 256 colors
    - ansi warnings because of incompatible colors
      (and possibily links) are suppressed
    
    TODO: Not sure if using .onLoad() this way is
    considered good practice. A "cleaner" away would
    be to define helper function which is capable
    of setting (and unsetting) the styles

--- R/zzz.R
+++ R/zzz.R
@@ -6,4 +6,18 @@
     register_s3("tbl_sum", "sc_meta", tbl_sum.sc_meta)
     register_s3("tbl_sum", "sc_tibble", tbl_sum.sc_tibble)
   }
+
+  if (in_pkgdown()) {
+    options(cli.theme = list(
+      ".field" = list("color" = "#0d0d73"),
+      ".code" = list("color" = "blue"),
+      ".fun" = list("color" = "#350b8e"),
+      "span.version" = list("color" = "#910808"),
+      "span.timestamp" = list("color" = "#430674")
+    ))
+    options(cli.hyperlink = TRUE)
+    options("cli.num_colors" = 16777216L)
+    options(fansi.warn = FALSE)
+    Sys.setenv(R_CLI_HYPERLINK_MODE = "posix")
+  }
 }
