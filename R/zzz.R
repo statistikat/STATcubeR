7094b022f698307deab6aca71e0b5652336f930d
Gregor de Cillia <de.cillia.gregor@gmail.com>
2022-09-22 16:17:03 +0200

    update cli theme for pkgdown
    - add helper fuctions to set/reset the theme
    - don't override cli.num_colors and
      cli.hyperlink
    - customize styling for {.run}
    - remove links in {.file} because those local
      file links would not work anyways

--- R/zzz.R
+++ R/zzz.R
@@ -7,17 +7,28 @@
     register_s3("tbl_sum", "sc_tibble", tbl_sum.sc_tibble)
   }
 
-  if (in_pkgdown()) {
-    options(cli.theme = list(
-      ".field" = list("color" = "#0d0d73"),
-      ".code" = list("color" = "blue"),
-      ".fun" = list("color" = "#350b8e"),
-      "span.version" = list("color" = "#910808"),
-      "span.timestamp" = list("color" = "#430674")
-    ))
-    options(cli.hyperlink = TRUE)
-    options("cli.num_colors" = 16777216L)
-    options(fansi.warn = FALSE)
-    Sys.setenv(R_CLI_HYPERLINK_MODE = "posix")
-  }
+  if (in_pkgdown())
+    cli_theme_pkgdown()
+}
+
+cli_theme_pkgdown <- function() {
+  options(cli.theme = list(
+    ".field" = list("color" = "#0d0d73"),
+    ".code" = list("color" = "blue"),
+    ".fun" = list("color" = "#350b8e"),
+    "span.version" = list("color" = "#910808"),
+    "span.timestamp" = list("color" = "#430674"),
+    ".run" = list(color = "#910808"),
+    ".file" = list(color = "#004700", transform = function(x) shQuote(x, "cmd"))
+  ))
+  options(fansi.warn = FALSE)
+  Sys.setenv(R_CLI_HYPERLINK_MODE = "posix")
+  cli::stop_app()
+}
+
+cli_theme_reset <- function() {
+  Sys.unsetenv("R_CLI_HYPERLINK_MODE")
+  options(cli.theme = NULL)
+  options(fansi.warn = NULL)
+  cli::stop_app()
 }
