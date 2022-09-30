5a08065d8bd9475020e8233f7947f258991b1da6
Gregor de Cillia <de.cillia.gregor@gmail.com>
2022-09-30 18:28:16 +0200

    add helper function for user agent
    [skip ci]

--- R/utils.R
+++ R/utils.R
@@ -14,15 +14,19 @@ sc_parse_time <- function(timestamp) {
   (as.numeric(timestamp) / 1000) %>% as.POSIXct(origin = "1970-01-01")
 }
 
+sc_user_agent <- function(){
+  paste0("STATcubeR/", sc_version(FALSE),
+         " (http://github.com/statistikat/STATcubeR)",
+         " httr/", utils::packageVersion("httr"),
+         " R/", R.version$major, ".", R.version$minor)
+}
+
 sc_headers <- function(language = NULL, key = NULL, server = "ext", ...) {
   if (is.null(key))
     key <- sc_key(server)
   httr::add_headers(
     APIKey = key, `Accept-Language` = sc_language(language), ...,
-    `User-Agent` = paste0("STATcubeR/", sc_version(FALSE),
-                          " (http://github.com/statistikat/STATcubeR)",
-                          " httr/", utils::packageVersion("httr"),
-                          " R/", R.version$major, ".", R.version$minor))
+    `User-Agent` = sc_user_agent())
 }
 
 sc_language <- function(language = NULL, options = c("en", "de")) {
