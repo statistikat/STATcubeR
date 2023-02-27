e23601cb7f4ff24a8a68063bbf5d17f9ccf6a505
Gregor de Cillia <de.cillia.gregor@gmail.com>
2023-02-27 07:01:52 +0100

    add custom color palette for @examples
    the current palette of colors for
    sc_schema is only suited for dark
    editors. For now, add a light theme
    for the pkgdown reference pages
    
    TODO: once the annotation printing
    is implemented, think about adding
    a way to customize the color scheme
    in all STATcubeR. This could also
    take care of cli_theme_pkgdown()
    
    [ci skip]

--- vignettes/R/setup.R
+++ vignettes/R/setup.R
@@ -1,4 +1,12 @@
 pkgload::load_all(export_all = FALSE, reset = FALSE, helpers = FALSE, quiet = TRUE)
 source("R/df_print.R")
 knitr::opts_chunk$set(comment = "#>")
+options(
+  STATcubeR.schema_colors = list(
+    "FOLDER" = "#4400cc", "DATABASE" = "#186868", "TABLE" = "#624918",
+    "GROUP" = "#4400cc", "FIELD" = "cyan", "VALUESET" = "cadetblue",
+    "VALUE" = "#4400cc", "MEASURE" = "#624918", "STAT_FUNCTION" = "cadetblue",
+    "COUNT" = "#624918"
+  )
+)
 source("R/add_tooltip.R")$value
