efeaf54662358ccd034e91289ddf9393daf0fd14
Alexander Kowarik <alex@kowarik.net>
2024-04-05 11:01:10 +0200

    remove pkgload dependency and library instead

--- vignettes/R/setup.R
+++ vignettes/R/setup.R
@@ -1,4 +1,4 @@
-pkgload::load_all(export_all = FALSE, reset = FALSE, helpers = FALSE, quiet = TRUE)
+library(STATcubeR)
 source("R/df_print.R")
 knitr::opts_chunk$set(comment = "#>")
 source("R/add_tooltip.R")$value
